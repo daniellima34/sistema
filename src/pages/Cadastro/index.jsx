3f33b8bf4808e98df4f0833fd0444578018ff729
daniellima34 <danielgomes29031991@gmail.comm>
2025-05-02 11:53:02 -0300

    atualizaão

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,5 +1,5 @@
 export default function CadastroPage(){
     return (
-        <p>Aqui é a pagina de login</p>
+      <p cadastrar> </p>
     )
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,16 @@
-export default function LoginPage(){
-return(
-    <p>Aqui é a pagina de login</p>
+import { Link } from "react-router-dom"
+import svg from "/public/mobile login-bro.svg"
 
-)
+export default function LoginPage(){
+    return (
+        <div className="w-full h-screen flex">
+            <div className="w-[40%] h-full flex items-center justify-center">
+                <Link to="/cadastro">cadastrar</Link>
+            </div>
+            <div className="w-[60%] h-full bg-[#6366EE] flex items-center justify-center rounded-l-lg flex-col">
+                <img width={600} src={svg} all="imagem animada" />
+                <p className="text-white font-bold w-[300px] text-[20px] text-center " A melhor experiencia de login que você já teve na sua vida></p>
+            </div>
+        </div>
+    ) 
 }
